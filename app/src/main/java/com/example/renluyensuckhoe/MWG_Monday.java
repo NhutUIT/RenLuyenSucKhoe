da44ddbd3c68bc5d58370fc01ffe790a8b967103
NhutUIT <45163363+NhutUIT@users.noreply.github.com>
2018-11-19 16:49:00 +0700

    RenLuyenSucKhoe

---
+++ app/src/main/java/com/example/renluyensuckhoe/MWG_Monday.java
@@ -0,0 +1,13 @@
+package com.example.renluyensuckhoe;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+public class MWG_Monday extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.acctivity_mwg_monday);
+    }
+}
---
+++ app/src/main/java/com/example/renluyensuckhoe/MenWeightLoss.java
@@ -0,0 +1,85 @@
+package com.example.renluyensuckhoe;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+public class MenWeightLoss extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menweightloss);
+
+        //Thứ 2
+        Button button1 = (Button) findViewById(R.id.bt1);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent1 = new Intent(MenWeightLoss.this,MWL_Monday.class);
+                startActivity(intent1);
+            }
+        });
+
+        //Thứ 3
+        Button button2 = (Button) findViewById(R.id.bt1);
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent2 = new Intent(MenWeightLoss.this,MWL_Tursday.class);
+                startActivity(intent2);
+            }
+        });
+
+        //Thứ 4
+        Button button3 = (Button) findViewById(R.id.bt1);
+        button3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent3 = new Intent(MenWeightLoss.this,MWL_Wednesday.class);
+                startActivity(intent3);
+            }
+        });
+
+        //Thứ 5
+        Button button4 = (Button) findViewById(R.id.bt1);
+        button4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent4 = new Intent(MenWeightLoss.this,MWL_Thursday.class);
+                startActivity(intent4);
+            }
+        });
+
+        //Thứ 6
+        Button button5 = (Button) findViewById(R.id.bt1);
+        button5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent5 = new Intent(MenWeightLoss.this,MWL_Friday.class);
+                startActivity(intent5);
+            }
+        });
+
+        //Thứ 7
+        Button button6 = (Button) findViewById(R.id.bt1);
+        button6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent6 = new Intent(MenWeightLoss.this,MWL_Saturday.class);
+                startActivity(intent6);
+            }
+        });
+        //Chủ Nhật
+        Button button7 = (Button) findViewById(R.id.bt1);
+        button7.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent7 = new Intent(MenWeightLoss.this,MWL_Sunday.class);
+                startActivity(intent7);
+            }
+        });
+    }
+}
